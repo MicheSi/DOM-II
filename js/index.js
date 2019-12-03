ac6bd6b3ea1b273991a595d1a6de5e7bbdc6734a
mlazzell17@gmail.com <MicheSi>
2019-12-03 13:56:57 -0700

    added events 5, 6, & 7

--- js/index.js
+++ js/index.js
@@ -23,11 +23,30 @@ background.addEventListener('wheel', () => {
     background.style.backgroundColor = 'rgba(176, 196, 222, .2)';
 })
 
-//Event 5
+//Event 5 - Mouse Move
+const headings = document.querySelectorAll('h2')
+headings.forEach(heading => {
+    heading.addEventListener('mousemove', () => {
+        heading.style.color = 'orange';
+    })
+})
 
-//Event 6
+//Event 6 - Mouse Over
+const buttons = document.querySelectorAll('.btn')
+buttons.forEach(button => {
+    button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.25)'
+        button.style.backgroundColor = 'yellow'
+    })
+})
 
-//Event 7
+//Event 7 - Mouse Out
+buttons.forEach(button => {
+    button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)'
+        button.style.backgroundColor = '#17A2B8'
+    })
+})
 
 //Event 8
 
