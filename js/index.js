d766820660b0ace8936ff98ad597e64941d8412f
mlazzell17@gmail.com <MicheSi>
2019-12-03 13:23:21 -0700

    added 2 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const moveBus = document.querySelector('img')
+moveBus.addEventListener('dblclick', () => {
+    moveBus.style.transform = "scale(.75)";
+})
+
+const navBackground = document.querySelector('.main-navigation')
+navBackground.addEventListener('mouseenter', () => {
+    navBackground.style.backgroundColor = 'lightgray';
+})
