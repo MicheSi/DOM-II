f900d878084f0f121e89c3f61cb4eb9b7854b752
mlazzell17@gmail.com <MicheSi>
2019-12-03 13:35:55 -0700

    added 3rd and 4th events

--- js/index.js
+++ js/index.js
@@ -1,10 +1,36 @@
 // Your code goes here
+
+//Event 1 - Double Click
 const moveBus = document.querySelector('img')
 moveBus.addEventListener('dblclick', () => {
     moveBus.style.transform = "scale(.75)";
 })
 
+//Event 2 - Mouse Enter
 const navBackground = document.querySelector('.main-navigation')
 navBackground.addEventListener('mouseenter', () => {
     navBackground.style.backgroundColor = 'lightgray';
 })
+
+//Event 3 - Mouse Leave
+navBackground.addEventListener('mouseleave', () => {
+    navBackground.style.backgroundColor = 'white';
+})
+
+//Event 4 - Wheel
+const background = document.querySelector('body')
+background.addEventListener('wheel', () => {
+    background.style.backgroundColor = 'rgba(176, 196, 222, .2)';
+})
+
+//Event 5
+
+//Event 6
+
+//Event 7
+
+//Event 8
+
+//Event 9
+
+//Event 10
